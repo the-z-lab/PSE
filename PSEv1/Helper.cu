fa26e0627dd55199c5958c18f594cb323682edc3
UdonCrab <hz322@uw.edu>
2025-04-14 16:09:29 -0700

    print partial_sum and temp from DotStepOne

--- PSEv1/Helper.cu
+++ PSEv1/Helper.cu
@@ -163,6 +163,7 @@ __global__ void gpu_stokes_DotStepOne_kernel(
 		Scalar3 b = make_scalar3(b4.x, b4.y, b4.z);
 
 		temp = dot(a,b); // Partial sum, each thread, shared memory
+		printf("temp = %f \n", temp)
 
 	}
 	else {
@@ -180,6 +181,7 @@ __global__ void gpu_stokes_DotStepOne_kernel(
         	if (threadIdx.x < offs)
             	{
             		partial_sum[threadIdx.x] += partial_sum[threadIdx.x + offs];
+					printf("Thread %d: partial_sum[%d] = %f \n", threadIdx.x, threadIdx.x + offs, partial_sum[threadIdx.x + offs]);
             	}
         	offs >>= 1;
         	__syncthreads();
